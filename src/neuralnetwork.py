129895447d472f87895676c2b8d485500d408742
Matheus Praxedes <matheuspraxedes17@hotmail.com>
2018-03-26 22:54:10 -0300

    Percepton adjustment

--- src/neuralnetwork.py
+++ src/neuralnetwork.py
@@ -3,12 +3,14 @@ import numpy as np
 class Perceptron:
 	"""docstring for ClassName"""
 	
+	v = 0.0
 	output = 0.0
-	derived_function
-	local_gradient
-	activation_function
-	weight_list
-	old_weight_list
+	derived_function = 0.0
+	local_gradient = 0.0
+	activation_function = 0.0
+	input_list = []
+	weight_list = []
+	old_weight_list = []
 	
 	def __init__(self, number_input, activation_function, derived_function):
 		
@@ -19,23 +21,34 @@ class Perceptron:
 		self.derived_function = derived_function
 	
 	def getOutput(self):
+		
 		return output
 
 	def process(self, input_list):
-	    input_list = [1.0] + input_list 
-	    np.multply(input_list, weight_list)
+		
+		input_list = [1.0] + input_list 
+		self.input_list = input_list
+		mul = np.multply(input_list, weight_list)
+		v = np.sum(mul)
+
+		output = activation_function(v)
 
 	def getWeight(self, index):
+		
 		return weight_list[index]
 
 	def getLocalGradient(self):
+		
 		return local_gradient
 	
 	def updateLocalGradient(self, sum):
-		pass
+		
+		return derived_function(v)*sum
 
-	def weightAdjustment(self):
-		pass
+	def weightAdjustment(self, learning_rate):
+		
+		old_weight_list = weight_list
+		weight_list = np.sum(weight_list, np.multply(learning_rate*local_gradient, input_list))  
 
 
 		

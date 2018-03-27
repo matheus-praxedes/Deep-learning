93b81ed4a9f848a0b7760b1cb5643aed7bd42f31
Matheus Praxedes <matheuspraxedes17@hotmail.com>
2018-03-26 23:33:23 -0300

    Testing perceptron

--- src/neuralnetwork.py
+++ src/neuralnetwork.py
@@ -3,22 +3,17 @@ import numpy as np
 class Perceptron:
 	"""docstring for ClassName"""
 	
-	v = 0.0
-	output = 0.0
-	derived_function = 0.0
-	local_gradient = 0.0
-	activation_function = 0.0
-	input_list = []
-	weight_list = []
-	old_weight_list = []
-	
 	def __init__(self, number_input, activation_function, derived_function):
 		
-		weight_list = np.random.random_sample(number_input)
-		old_weight_list = []
+		self.weight_list = np.random.random_sample(number_input)
+		self.old_weight_list = []
+		self.input_test = []
 		
 		self.activation_function = activation_function
 		self.derived_function = derived_function
+		self.local_gradient = 0.0
+		self.v = 0.0
+		self.output = 0.0
 	
 	def getOutput(self):
 		
@@ -28,7 +23,7 @@ class Perceptron:
 		
 		input_list = [1.0] + input_list 
 		self.input_list = input_list
-		mul = np.multply(input_list, weight_list)
+		mul = np.multiply(input_list, weight_list)
 		v = np.sum(mul)
 
 		output = activation_function(v)
@@ -51,5 +46,27 @@ class Perceptron:
 		weight_list = np.sum(weight_list, np.multply(learning_rate*local_gradient, input_list))  
 
 
-		
-		
+
+
+
+def relu(x):
+	
+	return np.fmax(0.0, x)		
+
+def derived_relu():
+	
+	if x >= 0:
+		return 1.0
+	else:
+		return 0.0	
+ 		
+
+input_test = [10.0, 8.0, 25.0]
+test = Perceptron(3, relu, derived_relu)
+test.process(input_test)
+
+for x in range(0,3):
+    print ("input", input_test[x], "weight", test.getWeight(x))
+
+
+print("Output: ",test.getOutput())
